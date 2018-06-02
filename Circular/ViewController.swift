8330f0463718009a5e5d91efe8dc49cee17d5fd4
Sasan Soroush <sasansoroush@Sasans-MacBook-Pro.local>
2018-06-03 00:32:11 +0430

    score system

--- Circular/ViewController.swift
+++ Circular/ViewController.swift
@@ -6,9 +6,17 @@
 //  Copyright © 1397 AP Sasan Soroush. All rights reserved.
 //
 
+/* IDEA : like these app but wheel on top
+    and every block has a number that we have to shoot the ball to same color block then it goes into the circle and starts to destroy and decrese the number like ballZ game other wise it will bounce outside the circle and it will increase the numbers
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var topIndex : Int = 0
+    var ballIndex : Int = 0
+    var score : Int = 0
     
     let colors : [UIColor] = [
         UIColor(rgb: 0xFC31AB) ,
@@ -31,13 +39,40 @@ class ViewController: UIViewController {
         let touch = touches.first!
         let location = touch.location(in: self.view)
         if location.x > view.frame.width/2 {
+            changeIndex(increase: true)
             rotateRight()
         } else {
+            changeIndex(increase: false)
             rotateLeft()
         }
     }
     
+    private func changeIndex(increase : Bool) {
+        if increase {
+            if topIndex == 0 {
+                topIndex = 7
+            } else {
+                topIndex -= 1
+            }
+        } else {
+            if topIndex == 7 {
+                topIndex = 0
+            } else {
+                topIndex += 1
+            }
+        }
+    }
+    
     // Properties UI
+    
+    let scoreLabel : UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = UIColor.init(rgb: 0x5A3D2B)
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.text = "0"
+        return label
+    }()
     
     let ball : UIView = {
         let view = UIView()
@@ -60,8 +95,8 @@ class ViewController: UIViewController {
         rotateView(targetView: wheel, piDividedBy: -4)
     }
     
-    private func rotateView(targetView: UIView, duration: Double = 0.1 , piDividedBy : Double = 4) {
-        UIView.animate(withDuration: duration, delay: 0.0, options: .curveLinear, animations: {
+    private func rotateView(targetView: UIView, duration: Double = 0.085 , piDividedBy : Double = 4) {
+        UIView.animate(withDuration: duration, delay: 0.0, options: .curveEaseInOut, animations: {
             targetView.transform = targetView.transform.rotated(by: CGFloat(Double.pi/piDividedBy))
         })
     }
@@ -75,27 +110,46 @@ class ViewController: UIViewController {
     }
     
     @objc private func ballFall() {
+        var Correct : Bool = false
         makeBall()
-        UIView.animate(withDuration: 2.5, animations: {
+        UIView.animate(withDuration: 2.0, animations: {
             
             self.ball.frame = CGRect(x: self.view.frame.width/2 - 12.5, y: self.wheel.center.y - 12.5, width: 25, height: 25)
-            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.3) {
+                if self.ballIndex == self.topIndex {
+                    Correct = true
+                } else {
+                    Correct = false
+                }
+            }
         }) { (animated) in
             
-            let originalTransform = self.ball.transform
-
-
-            UIView.animate(withDuration: 0.2, animations: {
-                let scaledTransform = originalTransform.scaledBy(x: 5, y: 5)
-                self.ball.transform = scaledTransform
-                self.ball.alpha = 0
-
-            }, completion: { (animated) in
-                self.ball.transform = originalTransform
-                self.ball.removeFromSuperview()
-
-            })
-            
+            if Correct  {
+                
+                self.score += 1
+                self.scoreLabel.text = String(self.score)
+                
+                let originalTransform = self.ball.transform
+                
+                UIView.animate(withDuration: 0.2, animations: {
+                    let scaledTransform = originalTransform.scaledBy(x: 5, y: 5)
+                    self.ball.transform = scaledTransform
+                    self.ball.alpha = 0
+                    
+                }, completion: { (animated) in
+                    self.ball.transform = originalTransform
+                    self.ball.removeFromSuperview()
+                })
+            }
+            else {
+                self.score = 0
+                self.scoreLabel.text = "0"
+                UIView.animate(withDuration: 1, animations: {
+                    self.ball.frame = CGRect(x: 50, y: self.view.frame.height/8, width: self.view.frame.width-100, height: self.view.frame.height/8)
+                }, completion: { (animated) in
+                    
+                })
+            }
         }
     }
     
@@ -105,17 +159,18 @@ class ViewController: UIViewController {
         ball.frame = CGRect(x: view.frame.width/2 - 12.5, y: view.frame.height/16, width: 25, height: 25)
         ball.layer.cornerRadius = 12.5
         let randomNumber = arc4random_uniform(7)
-//        UIView.animate(withDuration: 0.5) {
-//            self.view.backgroundColor = self.colors[Int(randomNumber)]
-//        }
+        ballIndex = Int(randomNumber)
         ball.backgroundColor = self.colors[Int(randomNumber)]
     }
     
     private func setupView() {
-        view.backgroundColor = UIColor.init(red: 40/255, green: 50/255, blue: 70/255, alpha: 1)
-//        view.backgroundColor = .white
+        view.backgroundColor = UIColor.init(rgb: 0xffecb4)
+        
         view.addSubview(wheel)
+        view.addSubview(scoreLabel)
+        
         wheel.frame = CGRect(x: 0, y: view.frame.height/2, width: view.frame.width, height: view.frame.height/2)
+        scoreLabel.frame = wheel.frame
         
         rotateView(targetView: wheel, duration: 0, piDividedBy: 8)
     }
